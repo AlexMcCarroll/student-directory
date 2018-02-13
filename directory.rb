225377cbad4459cb94d8ddc0b2a97f6f4735d0be
AlexMcCarroll <alexandra@mccarrollhk.com>
2018-02-13 15:04:35 +0000

    names indexed

--- directory.rb
+++ directory.rb
@@ -1,18 +1,5 @@
+
 =begin
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-=end
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -32,6 +19,21 @@ def input_students
   students
 
 end
+=end
+
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
 
 def print_header
   puts "The Students of Villans Academy"
@@ -39,7 +41,8 @@ def print_header
 end
 
 def print_body(names)
-  names.each_with_index do |x, index| puts "#{index + 1}. #{x[:name]} (#{x[:cohort]} cohort)"
+  names.each_with_index do |x, index| puts "#{index + 1}. #{x[:name]}
+    (#{x[:cohort]} cohort)"
   end
 end
 
@@ -47,7 +50,6 @@ def print_footer(names)
   puts "Overall we have #{names.count} great students"
 end
 
-students = input_students
 print_header
 print_body(students)
 print_footer(students)
