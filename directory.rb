dab58983a04425f0a33b0f04d21c08932d5a51ac
AlexMcCarroll <alexandra@mccarrollhk.com>
2018-02-13 13:12:43 +0000

    using hashes

--- directory.rb
+++ directory.rb
@@ -1,16 +1,31 @@
-puts "The Students of Villans Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall we have "
-print 11
-puts " great students"
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
+def print_header
+  puts "The Students of Villans Academy"
+  puts "-------------"
+end
+
+def print_body(names)
+  names.each do |x| puts "#{x[:name]} (#{x[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall we have #{names.count} great students"
+end
+
+print_header
+print_body(students)
+print_footer(students)
